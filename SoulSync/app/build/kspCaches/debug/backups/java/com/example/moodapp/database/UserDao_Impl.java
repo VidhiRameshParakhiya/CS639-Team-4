9d03efcf1d1a028c5bbb1f56b2dd0c16c5971b43
Yash Panchani <yashpanchani2002@gmail.com>
2024-12-06 22:53:05 -0500

    push my changes

---
+++ SoulSync/app/build/kspCaches/debug/backups/java/com/example/moodapp/database/UserDao_Impl.java
@@ -0,0 +1,139 @@
+package com.example.moodapp.database;
+
+import android.database.Cursor;
+import android.os.CancellationSignal;
+import androidx.room.CoroutinesRoom;
+import androidx.room.EntityInsertionAdapter;
+import androidx.room.RoomDatabase;
+import androidx.room.RoomSQLiteQuery;
+import androidx.room.util.CursorUtil;
+import androidx.room.util.DBUtil;
+import androidx.sqlite.db.SupportSQLiteStatement;
+import java.lang.Class;
+import java.lang.Exception;
+import java.lang.Integer;
+import java.lang.Object;
+import java.lang.Override;
+import java.lang.String;
+import java.lang.SuppressWarnings;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Callable;
+import kotlin.Unit;
+import kotlin.coroutines.Continuation;
+
+@SuppressWarnings({"unchecked", "deprecation"})
+public final class UserDao_Impl implements UserDao {
+  private final RoomDatabase __db;
+
+  private final EntityInsertionAdapter<User> __insertionAdapterOfUser;
+
+  public UserDao_Impl(RoomDatabase __db) {
+    this.__db = __db;
+    this.__insertionAdapterOfUser = new EntityInsertionAdapter<User>(__db) {
+      @Override
+      public String createQuery() {
+        return "INSERT OR ABORT INTO `users` (`id`,`username`,`password`) VALUES (?,?,?)";
+      }
+
+      @Override
+      public void bind(SupportSQLiteStatement stmt, User value) {
+        if (value.getId() == null) {
+          stmt.bindNull(1);
+        } else {
+          stmt.bindLong(1, value.getId());
+        }
+        if (value.getUsername() == null) {
+          stmt.bindNull(2);
+        } else {
+          stmt.bindString(2, value.getUsername());
+        }
+        if (value.getPassword() == null) {
+          stmt.bindNull(3);
+        } else {
+          stmt.bindString(3, value.getPassword());
+        }
+      }
+    };
+  }
+
+  @Override
+  public Object insertUser(final User user, final Continuation<? super Unit> continuation) {
+    return CoroutinesRoom.execute(__db, true, new Callable<Unit>() {
+      @Override
+      public Unit call() throws Exception {
+        __db.beginTransaction();
+        try {
+          __insertionAdapterOfUser.insert(user);
+          __db.setTransactionSuccessful();
+          return Unit.INSTANCE;
+        } finally {
+          __db.endTransaction();
+        }
+      }
+    }, continuation);
+  }
+
+  @Override
+  public Object getUser(final String username, final String password,
+      final Continuation<? super User> continuation) {
+    final String _sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+    final RoomSQLiteQuery _statement = RoomSQLiteQuery.acquire(_sql, 2);
+    int _argIndex = 1;
+    if (username == null) {
+      _statement.bindNull(_argIndex);
+    } else {
+      _statement.bindString(_argIndex, username);
+    }
+    _argIndex = 2;
+    if (password == null) {
+      _statement.bindNull(_argIndex);
+    } else {
+      _statement.bindString(_argIndex, password);
+    }
+    final CancellationSignal _cancellationSignal = DBUtil.createCancellationSignal();
+    return CoroutinesRoom.execute(__db, false, _cancellationSignal, new Callable<User>() {
+      @Override
+      public User call() throws Exception {
+        final Cursor _cursor = DBUtil.query(__db, _statement, false, null);
+        try {
+          final int _cursorIndexOfId = CursorUtil.getColumnIndexOrThrow(_cursor, "id");
+          final int _cursorIndexOfUsername = CursorUtil.getColumnIndexOrThrow(_cursor, "username");
+          final int _cursorIndexOfPassword = CursorUtil.getColumnIndexOrThrow(_cursor, "password");
+          final User _result;
+          if(_cursor.moveToFirst()) {
+            final Integer _tmpId;
+            if (_cursor.isNull(_cursorIndexOfId)) {
+              _tmpId = null;
+            } else {
+              _tmpId = _cursor.getInt(_cursorIndexOfId);
+            }
+            final String _tmpUsername;
+            if (_cursor.isNull(_cursorIndexOfUsername)) {
+              _tmpUsername = null;
+            } else {
+              _tmpUsername = _cursor.getString(_cursorIndexOfUsername);
+            }
+            final String _tmpPassword;
+            if (_cursor.isNull(_cursorIndexOfPassword)) {
+              _tmpPassword = null;
+            } else {
+              _tmpPassword = _cursor.getString(_cursorIndexOfPassword);
+            }
+            _result = new User(_tmpId,_tmpUsername,_tmpPassword);
+          } else {
+            _result = null;
+          }
+          return _result;
+        } finally {
+          _cursor.close();
+          _statement.release();
+        }
+      }
+    }, continuation);
+  }
+
+  public static List<Class<?>> getRequiredConverters() {
+    return Collections.emptyList();
+  }
+}
